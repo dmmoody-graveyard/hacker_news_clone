4b1f53bb86d7cf2a535f8b3c3cde715aec3d25ff
Duane Moody and Lee Jorgensen <dmmoody@gmail.com>
2015-02-25 13:58:05 -0800

    add home page

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-var hackNewsClone = angular.module('hackNewsClone', []);
+var hackNewsClone = angular.module('hackNewsClone', ['ui.router']);
+
+hackNewsClone.config(function($stateProvider){
+  $stateProvider.state('home', {
+    url:"",
+    templateUrl: "partials/home.html"
+  });
+});
