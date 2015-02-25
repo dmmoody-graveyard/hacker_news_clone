ce17962f1dc04d26d86eeb07c094657968e7318e
Duane Moody and Lee Jorgensen <dmmoody@gmail.com>
2015-02-25 14:58:29 -0800

    add new article with link

--- services/ArticleFactory.js
+++ services/ArticleFactory.js
@@ -2,8 +2,9 @@ hackNewsClone.factory('ArticleFactory', function ArticleFactory(){
   var factory = {};
   factory.articles = [];
   factory.addArticle = function() {
-    factory.articles.push({ name: factory.articleName, id: factory.articles.length + 1, comments: [] });
+    factory.articles.push({ name: factory.articleName, link: factory.articleLink, id: factory.articles.length + 1, comments: [] });
     factory.articleName = null;
+    factory.articleLink = null;
   };
 
   return factory;
